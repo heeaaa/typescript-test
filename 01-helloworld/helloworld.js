d6a9c93aab701dd0f14221193224fe2100947a5b
Hanna A <abejohanna@gmail.com>
2021-03-28 22:39:40 +0800

    Test commit

--- 01-helloworld/helloworld.js
+++ 01-helloworld/helloworld.js
@@ -1,12 +1,3 @@
 var firstName = 'Hanna';
 //firstName = 1;
 console.log("Hello, " + firstName);
-var reviews = [1, 2, 3, 4];
-for (var i = 0; i < reviews.length; i++) {
-    console.log("" + reviews[i]);
-}
-console.log('---------------------------------');
-for (var _i = 0, reviews_1 = reviews; _i < reviews_1.length; _i++) {
-    var i = reviews_1[_i];
-    console.log("" + i);
-}
